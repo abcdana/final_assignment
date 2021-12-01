c6d536b89b3c1b802b23f07f1c7b5b584dbab139
abcdana <74163296+abcdana@users.noreply.github.com>
2021-12-01 18:28:01 +0900

    Add login function using javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const id = document.getElementById('id'); // id를 입력할 수 있는 input 
+const password = document.getElementById('password'); //  password를 입력할 수 있는 input
+
+
+// id를 입력했을 때 && 비밀번호를 6자 이상 입력을 동시에 충족했을 때 로그인 버튼이 활성화(투명도 1)
+id.addEventListener('keyup', function(){
+    if(id.value !== "" && password.value.length > 5){
+         document.getElementById('login-btn').style.opacity = '1';
+    } // loginButton : 로그인 버튼
+});
+
+password.addEventListener('keyup', function(){
+    if(id.value !== "" && password.value.length > 5){
+         document.getElementById('login-btn').style.opacity = '1';
+    }
+});
+
